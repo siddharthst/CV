213ff217429f82fccefedf40b07ff3316b53a6a1
Siddharth Tomar <37268732+siddharthst@users.noreply.github.com>
2019-02-05 15:03:24 +0100

    Update latex_build.sh

--- latex_build.sh
+++ latex_build.sh
@@ -5,7 +5,7 @@
 
  sudo apt-get -qq update
  export PATH=/tmp/texlive/bin/x86_64-linux:$PATH
-
+ export PATH=/usr/local/texlive/2018/bin/x86_64-linux:$PATH
  if ! command -v pdflatex > /dev/null; then
      echo "Texlive not installed"
      echo "Downloading texlive and installing"
